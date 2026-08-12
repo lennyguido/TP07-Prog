1d47385dafac3160203fbfc260c8fbf9d1a75148
LENNY GUIDO BERCOVICH <49757661@ort>
2026-08-12 08:36:14 -0300

    First

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Nueva_carpeta.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
